4d28bd3e79d6ebbc5361e93354a3dbf4e985ae6a
Jamal Ahmed <j8ahmed@gmail.com>
2018-11-24 16:04:52 -0500

    Finished the project, updating to match my glitch project.

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,4 @@
+
 /*
 *
 *
@@ -17,7 +18,93 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      let query = req.query;
+      console.log(query);
+      let stock = req.query.stock;
+      let like  = req.query.like;
+      //Check to see if the query is for one stock or two
+      if(typeof stock != typeof []){
+        console.log('Bring it in people! We are only dealing with one stock. This should be easy!!');
+        stock = stock.toUpperCase();
+        //Check if we need to update the likes for the stock
+        if(!like){
+          //We do not need to update the document find and present the stock
+          MongoClient.connect(CONNECTION_STRING, function(err, db) {
+              db.collection('stocks').findOne({stock: stock}, (err, result)=>{
+              if(err) return console.log(err);
+              
+              // console.log(result);
+              res.json({
+                stockData: {
+                  stock: result.stock,
+                  price: result.price,
+                  likes: result.likes
+                }
+              });
+            }); 
+          });
+        }else{
+          //We need to update the likes
+          MongoClient.connect(CONNECTION_STRING, function(err, db) {
+
+            db.collection('stocks').findOneAndUpdate({stock: stock}, { $inc: { likes: 1}}, {returnNewDocument: true},(err, result)=>{
+              if(err) return console.log(err);
+              
+              console.log(result.value);
+              res.json({
+                stockData: {
+                  stock: result.value.stock,
+                  price: result.value.price,
+                  likes: result.value.likes
+                }
+              });
+            });
+          });
+        }
+      }else{
+        //We are dealing with two stock values
+        console.log('Its an array everyone... Calm down, we just have to compare and present two stocks to the user.');
+        let stock1 = stock[0].toUpperCase();
+        let stock2 = stock[1].toUpperCase();
+        
+        //Check if we need to update the likes for both the stocks
+        if(!like){
+          MongoClient.connect(CONNECTION_STRING, function(err, db) {// 
+              db.collection('stocks').find({ $or: [ {stock: stock1}, {stock: stock2} ]}).toArray((err, result)=>{//                  if(err) return console.log(err); //                  console.log(result)
+                console.log(result);
+                res.json({
+                  stockData: [
+                    { stock: result[0].stock, price: result[0].price, rel_likes: (result[0].likes - result[1].likes) },
+                    { stock: result[1].stock, price: result[1].price, rel_likes: (result[1].likes - result[0].likes) }
+                  ]
+                });
+              }); 
+          });
+        }else{
+          MongoClient.connect(CONNECTION_STRING, function(err, db) {// 
+              //find and update the likes for stock 1
+              db.collection('stocks').update({ $or: [ {stock: stock1}, {stock: stock2} ]}, {$inc: {likes:1} }, {multi: true}, (err, result)=>{
+                if(err) return console.log(err);
+              });
+              //find and update the likes for stock 2
+              db.collection('stocks').find({ $or: [ {stock: stock1}, {stock: stock2} ]}).toArray((err, result)=>{
+                if(err) return console.log(err);
+                console.log(result);
+                res.json({
+                  stockData: [
+                    { stock: result[0].stock, price: result[0].price, rel_likes: (result[0].likes - result[1].likes) },
+                    { stock: result[1].stock, price: result[1].price, rel_likes: (result[1].likes - result[0].likes) }
+                  ]
+                });
+                
+              });
+          });
+        }
+        
+        
+      }
+    
     });
     
+
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -13,6 +13,8 @@ var server = require('../server');
 
 chai.use(chaiHttp);
 
+let likes = 0;
+
 suite('Functional Tests', function() {
     
     suite('GET /api/stock-prices => stockData object', function() {
@@ -22,27 +24,91 @@ suite('Functional Tests', function() {
         .get('/api/stock-prices')
         .query({stock: 'goog'})
         .end(function(err, res){
-          
-          //complete this one too
-          
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.property(res.body.stockData, 'stock');
+          assert.equal(res.body.stockData.stock, 'GOOG');
+          assert.property(res.body.stockData, 'price');
+          assert.property(res.body.stockData, 'likes');
+          likes = res.body.stockData.likes;
           done();
         });
       });
       
       test('1 stock with like', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({stock: 'goog', like: true})
+          .end((err, res)=>{
+            assert.equal(res.status, 200);
+            assert.isObject(res.body);
+            assert.property(res.body.stockData, 'stock');
+            assert.equal(res.body.stockData.stock, 'GOOG');
+            assert.property(res.body.stockData, 'price');
+            assert.property(res.body.stockData, 'likes');
+            assert.isAbove(res.body.stockData.likes, likes);
+            likes = res.body.stockData.likes;
+            done();
+          });
       });
       
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({stock: 'goog', like: true})
+          .end((err, res)=>{
+            assert.equal(res.status, 200);
+            assert.isObject(res.body);
+            assert.property(res.body.stockData, 'stock');
+            assert.equal(res.body.stockData.stock, 'GOOG');
+            assert.property(res.body.stockData, 'price');
+            assert.property(res.body.stockData, 'likes');
+            assert.equal(res.body.stockData.likes, likes);
+            done();
+          });
       });
       
+      let rel_likes;
+      
       test('2 stocks', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({stock: ['goog', 'msft']})
+          .end((err, res)=>{
+            assert.equal(res.status, 200);
+            assert.isObject(res.body);
+            assert.property(res.body.stockData[0], 'stock');
+            assert.property(res.body.stockData[0], 'rel_likes');
+            assert.property(res.body.stockData[0], 'price');
+            assert.property(res.body.stockData[1], 'stock');
+            assert.property(res.body.stockData[1], 'rel_likes');
+            assert.property(res.body.stockData[1], 'price');
+            assert.oneOf(res.body.stockData[0].stock, ['GOOG','MSFT']);
+            assert.oneOf(res.body.stockData[1].stock, ['GOOG','MSFT']);
+            assert.equal(res.body.stockData[0].rel_likes + res.body.stockData[1].rel_likes, 0);
+            rel_likes = Math.abs(res.body.stockData[0].rel_likes);
+            done();
+          });
       });
       
       test('2 stocks with like', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({stock: ['goog','msft'], like: true})
+          .end((err, res)=>{
+            assert.equal(res.status, 200);
+            assert.isObject(res.body);
+            assert.property(res.body.stockData[0], 'stock');
+            assert.property(res.body.stockData[0], 'rel_likes');
+            assert.property(res.body.stockData[0], 'price');
+            assert.property(res.body.stockData[1], 'stock');
+            assert.property(res.body.stockData[1], 'rel_likes');
+            assert.property(res.body.stockData[1], 'price');
+            assert.oneOf(res.body.stockData[0].stock, ['GOOG','MSFT']);
+            assert.oneOf(res.body.stockData[1].stock, ['GOOG','MSFT']);
+            assert.equal(res.body.stockData[0].rel_likes + res.body.stockData[1].rel_likes, 0);
+            assert.equal(Math.abs(res.body.stockData[0].rel_likes), rel_likes + 1);
+          });
       });
       
     });
